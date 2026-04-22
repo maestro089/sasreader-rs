1cd1e886f52aeb9be8ce748ecd69088e9f1b8018
maestro089 <egor-karpov08@bk.ru>
2026-04-22 22:15:08 +0300

    ААААААААААААААААААААААААААААААААА

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 
 use std::fs::File;
 use std::io::{BufReader, Read};
+use std::io::Result;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
     let file = File::open("cars.sas7bdat")?;
     let mut reader = BufReader::new(file);
-    let mut header = [0u8; 288];
+    let mut header_byte = [0u8; 288];
 
-    reader.read_exact(&mut header)?;
+    reader.read_exact(&mut header_byte)?;
 
-    let encoding_code = u16::from_le_bytes([header[70], header[71]]);
-
-    println!("Код кодировки: {}", encoding_code);
+    // let header = SasReadHeaderFile{ file_byte: header_byte };
+    let mut header = SasReadHeaderFile::new(header_byte);
+    header.parse_metadata();
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct SASProperties{
+    encode: String,
+}
+
+struct SasReadHeaderFile{
+    file_byte: [u8; 288],
+    properties: SASProperties,
+}
+
+impl SasReadHeaderFile{
+    fn new(file_byte: [u8; 288]) -> Self{
+        Self {
+            file_byte, 
+            properties: SASProperties {
+                encode: "UTF-8".to_string(),
+            }
+        }
+    }
+
+    fn parse_metadata(&mut self){
+        let _encoding_code = u16::from_le_bytes([self.file_byte[70], self.file_byte[71]]);
+        self.properties.encode = "wqeqwe".to_string();
+        
+        println!("{:?}", self.properties);
+    }
 }
